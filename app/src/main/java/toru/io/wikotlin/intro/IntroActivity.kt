1c3f78c09a50d125e484a5f5362bfdc46e13b5c5
Toru Choi <toru@zikto.com>
2016-03-10 22:46:18 +0900

    test code

--- app/src/main/java/toru/io/wikotlin/intro/IntroActivity.kt
+++ app/src/main/java/toru/io/wikotlin/intro/IntroActivity.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.view.ViewCompat
+import android.support.v4.view.ViewPropertyAnimatorCompat
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.TextView
 import toru.io.wikotlin.R
 import toru.io.wikotlin.framework.ui.AbsBaseActivity
 import toru.io.wikotlin.main.MainActivity
@@ -18,7 +24,33 @@ class IntroActivity : AbsBaseActivity() {
             finish()}, 2000);
     }
 
-    override fun initView() {}
+    override fun initView() {
+//        var logoText: TextView = findViewById(R.id.logo_text) as TextView
+//        var btnGroup: ViewGroup = findViewById(R.id.container) as ViewGroup
+//
+//        ViewCompat.animate(logoText)
+//                    .translationY(-250.0f)
+//                    .setStartDelay(300)
+//                    .setDuration(1000)
+//                    .setInterpolator(DecelerateInterpolator(1.2f))
+//                    .start()
+//
+//        btnGroup.visibility = View.VISIBLE
+//
+//        for(i in 0..btnGroup.childCount-1){
+//            var btn = btnGroup.getChildAt(i)
+//            var viewAnimator:ViewPropertyAnimatorCompat
+//
+//            viewAnimator = ViewCompat.animate(btn)
+//                                        .translationY(-50.0f).alpha(1.0f)
+//                                        .setStartDelay(((300 * i) + 500).toLong())
+//                                        .setDuration(1000)
+//            viewAnimator.interpolator = DecelerateInterpolator()
+//            viewAnimator.start()
+//        }
+    }
+
+
 
     override fun initToolbar() {}
 
